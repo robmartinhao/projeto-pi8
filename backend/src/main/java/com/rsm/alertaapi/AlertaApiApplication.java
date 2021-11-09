662f8e4b284070aba1e7d4bc005a854788c5bf37
Robson <robmartinhao@gmail.com>
2021-11-08 21:26:51 -0300

    Initial commit

---
+++ backend/src/main/java/com/rsm/alertaapi/AlertaApiApplication.java
@@ -0,0 +1,13 @@
+package com.rsm.alertaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rsm/alertaapi/AlertaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsm.alertaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
