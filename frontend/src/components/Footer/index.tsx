2a5eb42ae1402b45b321ee243e82a314a11ebe8c
Robson <robmartinhao@gmail.com>
2021-11-11 19:29:19 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Projeto PI8</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
